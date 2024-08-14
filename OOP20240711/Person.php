a044977ed4cddbf739a6aea6c811de5c3073605c
tamagoage <116415023+tamagoage@users.noreply.github.com>
2024-08-14 12:26:38 +0900

    setup

---
+++ OOP20240711/Person.php
@@ -0,0 +1,23 @@
+<?php
+class Person {
+    public $name;
+    public $age;
+
+    public function __construct($name, $age) 
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function introduce() {
+        return "My name is {$this->name}. I am {$this->age} years old.";
+    }
+
+    public function celebrateBirthday() {
+        $this->age++;
+        return $this->age;
+    }
+}
+
+$person = new Person("John", 20);
+echo $person->celebrateBirthday();
